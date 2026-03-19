4afac8d39be9a83e405ac4565057f005fe5bca1f
Cecilia Grace <cecigrace2003@gmail.com>
2026-03-19 12:45:13 +0300

    added .js file

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,20 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if(temperature > 80) {
+    console.log('Watering on')
+} else if(temperature < 80) {
+    console.log('Watering off')
+}
+
+if(timeOfDay === 'morning') {
+    console.log('Lights off')
+} else {
+    console.log('Lights on')
+}
+
+while(soilMoisture < 41) {
+    console.log(soilMoisture)
+    soilMoisture++
+}
